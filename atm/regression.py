96b11b16614a37735dbacaa7fd44e8bf040716d0
Semyon-Yakovlev <patt1337gg@gmail.com>
2024-01-07 17:22:17 +0300

    one more

--- atm/regression.py
+++ atm/regression.py
@@ -1,6 +1,7 @@
-from catboost import CatBoostRegressor
+from catboost import CatBoostRegressor, Pool
 from joblib import dump
 from omegaconf import DictConfig
+from sklearn.model_selection import train_test_split
 
 from hydra import main
 
@@ -11,13 +12,23 @@ from .data import read_data
 @main(version_base=None, config_path="../hydra", config_name="config")
 def train(cfg: DictConfig):
     data = read_data(train_dir_git)
-    X = data.drop(columns=["target"])
-    y = data.target
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.drop(columns=["target"]),
+        data.target,
+        test_size=0.33,
+        random_state=cfg["params"].random,
+    )
+    val_pool = Pool(X_test, y_test)
     reg = CatBoostRegressor(
         iterations=cfg["params"].iterations,
         learning_rate=cfg["params"].learning_rate,
         loss_function=cfg["params"].loss_function,
-    ).fit(X, y)
+        verbose=cfg["params"].verbose,
+        random_seed=cfg["params"].random,
+        task_type=cfg["params"].task_type,
+    ).fit(
+        X_train, y_train, eval_set=val_pool, use_best_model=cfg["params"].use_best_model
+    )
     dump(reg, models_dir_local)
 
 
