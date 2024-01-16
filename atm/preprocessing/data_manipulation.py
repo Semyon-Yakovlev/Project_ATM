dfd70416d0176aade71d9ef9639628dbf2eedc8b
Semyon-Yakovlev <patt1337gg@gmail.com>
2024-01-17 01:17:47 +0300

    add preprocessing

--- atm/preprocessing/data_manipulation.py
+++ atm/preprocessing/data_manipulation.py
@@ -1,27 +1,37 @@
-from fire import Fire
-
-
 import json
 
 import numpy as np
 import pandas as pd
+from fire import Fire
 
-from . import train_dir_git, train_dir_local
 from ..data import read_data
+from . import train_dir_git, train_dir_local
 
 
 def calc_dist(lat_1, long_1, lat_2, long_2):
     lat_diff = lat_1 - lat_2
     long_diff = long_1 - long_2
-    distance = 6378.137 * 2 * np.arcsin(np.sqrt(np.sin(lat_diff/2.0)**2 + np.cos(lat_1) * np.cos(lat_2) * np.sin(long_diff/2.0)**2))
+    distance = (
+        6378.137
+        * 2
+        * np.arcsin(
+            np.sqrt(
+                np.sin(lat_diff / 2.0) ** 2
+                + np.cos(lat_1) * np.cos(lat_2) * np.sin(long_diff / 2.0) ** 2
+            )
+        )
+    )
     return distance
 
 
 def find_population(lat, long, set_data):
     distances = set_data.apply(
-        lambda settlement: calc_dist(lat, long, settlement['latitude_rad'], settlement['longitude_rad']),
-        axis=1)
-    return set_data.loc[distances.idxmin(), 'population']
+        lambda settlement: calc_dist(
+            lat, long, settlement["latitude_rad"], settlement["longitude_rad"]
+        ),
+        axis=1,
+    )
+    return set_data.loc[distances.idxmin(), "population"]
 
 
 def add_features():
@@ -78,25 +88,47 @@ def add_features():
             how="left",
         )
 
-        settlements = pd.read_csv('../data/settlements.csv', sep=';')
-        settlements = settlements[settlements['population'] >= 1000]
-        set_data = settlements[['region', 'settlement', 'latitude_dd', 'longitude_dd', 'population']].groupby(by=['region', 'settlement']).aggregate({'latitude_dd':'mean',
-                                                                                                          'longitude_dd':'mean',
-                                                                                                          'population':'sum'}).reset_index()
-        set_data[['latitude_rad', 'longitude_rad']] = np.radians(set_data[['latitude_dd', 'longitude_dd']])
-        train['population'] = train.apply(
-                        lambda row: find_population(row['lat_rad'], row['long_rad']),
-                        axis=1)
+        settlements = pd.read_csv("../data/settlements.csv", sep=";")
+        settlements = settlements[settlements["population"] >= 1000]
+        set_data = (
+            settlements[
+                ["region", "settlement", "latitude_dd", "longitude_dd", "population"]
+            ]
+            .groupby(by=["region", "settlement"])
+            .aggregate(
+                {"latitude_dd": "mean", "longitude_dd": "mean", "population": "sum"}
+            )
+            .reset_index()
+        )
+        set_data[["latitude_rad", "longitude_rad"]] = np.radians(
+            set_data[["latitude_dd", "longitude_dd"]]
+        )
+        train["population"] = train.apply(
+            lambda row: find_population(row["lat_rad"], row["long_rad"]), axis=1
+        )
 
-        density = pd.read_csv('../data/density.csv')
-        density['region'] = density['region'].str.lower()
-        train = train.merge(density, how='left', on='region')
+        density = pd.read_csv("../data/density.csv")
+        density["region"] = density["region"].str.lower()
+        train = train.merge(density, how="left", on="region")
 
-        salary = pd.read_csv('../data/salary.csv')
-        salary['region'] = salary['region'].str.lower()
-        train = train.merge(salary, how='left', on='region')
+        salary = pd.read_csv("../data/salary.csv")
+        salary["region"] = salary["region"].str.lower()
+        train = train.merge(salary, how="left", on="region")
 
-        train.drop(columns=['atm_id', 'id', 'atm_group', 'address', 'address_rus', 'lat_rad', 'long_rad', 'key', 'region'], inplace=True)
+        train.drop(
+            columns=[
+                "atm_id",
+                "id",
+                "atm_group",
+                "address",
+                "address_rus",
+                "lat_rad",
+                "long_rad",
+                "key",
+                "region",
+            ],
+            inplace=True,
+        )
 
     return train.to_csv(train_dir_local)
 
