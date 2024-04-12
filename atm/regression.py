358c317145ba7bc28f795e18f95b6249763636e2
Semyon-Yakovlev <patt1337gg@gmail.com>
2024-04-12 21:57:33 +0300

    fix pre-commit;

--- atm/regression.py
+++ atm/regression.py
@@ -1,18 +1,19 @@
 import pickle
 from subprocess import check_output
 
-from hydra import main
 from mlflow import log_metric, log_param, set_tracking_uri, start_run
 from omegaconf import DictConfig
 from pandas import read_csv
 from sklearn.metrics import r2_score
 from xgboost import XGBRegressor
 
+from hydra import main
+
 from . import data_pipeline_dir_local, model_dir_local, test_dir_local, train_dir_local
 from .preprocessing.transform_data import transform_data
 
 
-@main(version_base=None, config_path="./hydra", config_name="config")
+@main(version_base=None, config_path="../hydra", config_name="config")
 def train(cfg: DictConfig):
     set_tracking_uri(f"""http://{cfg["params"].host}:{cfg["params"].port}""")
     git_commit_id = (
--- atm/services/fastapi/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-model_dir_local = Path.cwd() / "data" / "model.pkl"
-data_pipeline_dir_local = Path.cwd() / "data" / "data_pipeline.pkl"
-predictions_dir_local = Path.cwd() / "data" / "predictions.csv"
-feedback_dir_local = Path.cwd() / "data" / "feedback.csv"
